4a49499e3c8b45a029ebd91450d24b0dc0aff42a
guilhelp <guilhermelimapaula.dev@gmail.com>
2024-01-13 18:14:13 -0300

    portfolio

---
+++ src/js/app.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const card1 = document.querySelector(".card1");
+
+    card1.addEventListener("click", function () {
+        // Abre o link em uma nova página
+        window.open("https://github.com/guilhelp/Coopets", "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const card1 = document.querySelector(".card2");
+
+    card1.addEventListener("click", function () {
+        // Abre o link em uma nova página
+        window.open("https://github.com/guilhelp/ProjetoInterdisciplinar", "_blank");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const card1 = document.querySelector(".card3");
+
+    card1.addEventListener("click", function () {
+        // Abre o link em uma nova página
+        window.open("https://github.com/guilhelp/Nmap-Site", "_blank");
+    });
+});
